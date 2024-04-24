acd6290b7a9ecb475b4ebb116ef71e26cc6975f0
Claudia Ageha <claudiaageha@gmail.com>
2024-04-24 16:35:13 +0300

    tasks

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
 """
-start Flask application
+script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /: display "Hello HBNB!"
 """
 
 from flask import Flask
-app = Flask(__name__)
+
+app = Flask("__name__")
 
 
-@app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+@app.route("/airbnb-onepage/", strict_slashes=False)
+def hello_hbnb():
+    """display text"""
+    return "Hello HBNB!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
